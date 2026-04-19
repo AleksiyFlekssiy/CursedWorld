bd16c637e735ea14568584e9b1829c96ccf7ea6f
AleksiyFlekssiy <alex77y71@gmail.com>
2026-04-19 16:21:51 +0300

    feat(shikigami): Max Elephant's Water Splash use his viewXRot as pitch angle.

--- src/main/java/com/aleksiyflekssiy/cursedworld/entity/behavior/maxelephant/WaterSplash.java
+++ src/main/java/com/aleksiyflekssiy/cursedworld/entity/behavior/maxelephant/WaterSplash.java
@@ -1,10 +1,13 @@
 package com.aleksiyflekssiy.cursedworld.entity.behavior.maxelephant;
 
+import com.aleksiyflekssiy.cursedworld.client.renderer.CustomDebugRenderer;
 import com.aleksiyflekssiy.cursedworld.entity.MaxElephantEntity;
+import com.aleksiyflekssiy.cursedworld.network.AddAABBToRenderPacket;
 import com.aleksiyflekssiy.cursedworld.network.AddOBBToRenderPacket;
 import com.aleksiyflekssiy.cursedworld.network.ModMessages;
 import com.aleksiyflekssiy.cursedworld.phys.OBB;
 import com.aleksiyflekssiy.cursedworld.util.RotationUtil;
+import net.minecraft.commands.arguments.EntityAnchorArgument;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -43,7 +46,7 @@ public class WaterSplash extends Behavior<MaxElephantEntity> {
     protected boolean checkExtraStartConditions(ServerLevel level, MaxElephantEntity maxElephant) {
         if (maxElephant.getOwner() == null) return false;
         if (!maxElephant.getBrain().hasMemoryValue(MemoryModuleType.ATTACK_TARGET)) return false;
-        return true;
+        return maxElephant.distanceTo(maxElephant.getBrain().getMemory(MemoryModuleType.ATTACK_TARGET).get()) < 50;
     }
 
     @Override
@@ -54,34 +57,77 @@ public class WaterSplash extends Behavior<MaxElephantEntity> {
 
     @Override
     protected void start(ServerLevel level, MaxElephantEntity maxElephant, long pGameTime) {
-        System.out.println("Start");
         setOBB(maxElephant);
     }
 
     @Override
     protected void tick(ServerLevel level, MaxElephantEntity maxElephant, long pGameTime) {
-
         ModMessages.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) maxElephant.getOwner()), new AddOBBToRenderPacket(area, true));
-        float rot = maxElephant.getViewYRot(1) + 1;
-        maxElephant.setYBodyRot(rot);
-        maxElephant.setYRot(rot);
-        maxElephant.setYHeadRot(rot);
         waterTicks++;
-        ModMessages.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) maxElephant.getOwner()), new AddOBBToRenderPacket(area, false));
         setOBB(maxElephant);
-        List<Entity> entities = level.getEntities(maxElephant, area.toAABB());
+        AABB aabb = area.toAABB();
+        List<Entity> entities = level.getEntities(maxElephant, aabb);
+        ModMessages.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) maxElephant.getOwner()), new AddAABBToRenderPacket(aabb, true));
         entities.forEach(entity -> {
-                if (area.contains(entity.position())) entity.setDeltaMovement(maxElephant.getViewVector(1).scale(2));
-                });
+            Vec3 pos = entity.position();
+                    if (area.intersects(entity)) {
+                    entity.setDeltaMovement(maxElephant.getViewVector(1).scale(2));
+                    level.sendParticles(
+                            ParticleTypes.SONIC_BOOM,
+                            pos.x,
+                            pos.y,
+                            pos.z,
+                            1,
+                            0,0,0,0
+                    );
+                }
+                else {
+                        level.sendParticles(
+                                ParticleTypes.EXPLOSION,
+                                pos.x,
+                                pos.y,
+                                pos.z,
+                                1,
+                                0,0,0,0
+                        );
+                }
+        });
+        Vec3 min = new Vec3(
+                aabb.minX,
+                aabb.minY,
+                aabb.minZ
+        );
+        Vec3 max = new Vec3(
+                aabb.maxX,
+                aabb.maxY,
+                aabb.maxZ
+        );
+        level.sendParticles(
+                ParticleTypes.LARGE_SMOKE,
+                min.x,
+                min.y,
+                min.z,
+                1,
+                0,0,0,0
+        );
+        level.sendParticles(
+                ParticleTypes.LARGE_SMOKE,
+                max.x,
+                max.y,
+                max.z,
+                1,
+                0,0,0,0
+        );
     }
 
     private void setOBB(MaxElephantEntity maxElephant) {
+        maxElephant.lookAt(EntityAnchorArgument.Anchor.FEET, maxElephant.getBrain().getMemory(MemoryModuleType.ATTACK_TARGET).get().position());
         Vec3 center = maxElephant.position().add(RotationUtil.getOffsetLookPosition(maxElephant, maxElephant.getLookAngle(), 0, 0, 25));
 
         area = new OBB(
                 center.add(-5, 0, -25),
                 center.add(5, 3, 25),
-                new Vector3f(0, maxElephant.getViewYRot(1), 0)
+                new Vector3f(maxElephant.getViewXRot(1), maxElephant.getViewYRot(1), 0)
         );
     }
 
@@ -89,8 +135,9 @@ public class WaterSplash extends Behavior<MaxElephantEntity> {
     protected void stop(ServerLevel level, MaxElephantEntity maxElephant, long pGameTime) {
         waterTicks = 0;
         ModMessages.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) maxElephant.getOwner()), new AddOBBToRenderPacket(area, false));
+        CustomDebugRenderer.OBB_LIST.keySet().forEach(CustomDebugRenderer::removeOBB);
+        CustomDebugRenderer.AABB_LIST.keySet().forEach(CustomDebugRenderer::removeAABB);
         area = null;
         particlePositions.clear();
     }
-
 }
